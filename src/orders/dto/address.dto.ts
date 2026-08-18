f6dcffdf6a39bde0dd8e18b603a91056684d419a
Arpit <arpit.ptdr@hotmail.com>
2026-08-18 19:35:54 +0100

    remove unused class validator

--- src/orders/dto/address.dto.ts
+++ src/orders/dto/address.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsOptional, IsEmail, IsMobilePhone } from 'class-validator';
+import { IsString, IsOptional, IsEmail } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class AddressDto {
